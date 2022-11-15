01cab5fcbc728ec2f4eb6d0a9fa7b29f98397931
oldcapone <ivanov.planeta@gmail.com>
2022-11-15 21:20:57 +0300

    ADD Sqlite BD

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ from json import dump as jdump
 from requests import get
 from pycbrf import ExchangeRates
 
+from sqlite3 import connect
 from flask import Flask, render_template, request
 from hh_json import parce
+from crud import add_row
 
 app = Flask(__name__)
 
@@ -27,15 +29,20 @@ def contact():
 def form():
     return render_template('form.html')
 
-
 @app.post('/result/')
 def result():
+    """
+    Вычисление проекта и вывод страницы результатов
+    :return: шаблон с результатами
+    """
     vac = request.form
     data = parce(**vac)
-    dat = {**data, **vac}   # data | vac - в Python 3.10 можно сделать так
+    dat = {**data, **vac}
     print(dat)
+    dat['where'] = 'в названии вакансии' \
+        if dat['where'] == 'name' else 'в названии компании' if dat['where'] == 'company' else 'везде'
+    add_row(dat)
     return render_template('about.html', res=dat)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
